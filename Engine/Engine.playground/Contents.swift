be278af780eb75de3258c3be12dafb97c8ba4b1c
Todd Olsen <todd.olsen@me.com>
2016-09-25 22:39:02 -0400

    Add resources to playground.

--- Engine/Engine.playground/Contents.swift
+++ Engine/Engine.playground/Contents.swift
@@ -1,10 +1,20 @@
 import Engine
 import Foundation
 
-let url = Bundle.main.url(forResource: "mateIn1", withExtension: "epd")!
-let epds = try! String(contentsOf: url)
-    .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-    .components(separatedBy: "\r\n")
-    .flatMap { EPD(parse: $0) }
+//let url = Bundle.main.url(forResource: "mateIn1", withExtension: "epd")!
+//let epds = try! String(contentsOf: url)
+//    .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+//    .components(separatedBy: "\r\n")
+//    .flatMap { EPD(parse: $0) }
+//
+//print(epds)
 
-print(epds)
+let game = Game()
+try! game.execute(sanMoves: "e4 c5 Nc3 Nc6 g3")
+//print(game.eco)
+
+
+let eco = game.eco!.name
+    .replacingOccurrences(of: game.eco!.code.rawValue, with: "")
+    .trimmingCharacters(in: CharacterSet.whitespaces.union(CharacterSet.punctuationCharacters))
+print(">\(eco)<")
