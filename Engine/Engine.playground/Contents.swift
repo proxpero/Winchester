cef650f772a41599b96979e298c3643ee304c344
Todd Olsen <todd.olsen@me.com>
2016-09-28 10:25:32 -0400

    Update playground.

--- Engine/Engine.playground/Contents.swift
+++ Engine/Engine.playground/Contents.swift
@@ -1,20 +1,47 @@
 import Engine
 import Foundation
 
-//let url = Bundle.main.url(forResource: "mateIn1", withExtension: "epd")!
-//let epds = try! String(contentsOf: url)
-//    .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-//    .components(separatedBy: "\r\n")
-//    .flatMap { EPD(parse: $0) }
-//
-//print(epds)
+class Delegate: GameDelegate {
 
-let game = Game()
-try! game.execute(sanMoves: "e4 c5 Nc3 Nc6 g3")
-//print(game.eco)
+    func game(_: Game, didExecute move: Move, with capture: Capture?, with promotion: Piece?) {
+        print("move: \(move), capture: \(capture), promotion: \(promotion)")
+    }
+
+    func game(_: Game, didAdvance items: [HistoryItem]) {
+        print(#function)
+        print(items.count)
+    }
+
+    func game(_: Game, didReverse items: [HistoryItem]) {
+        print(#function)
+        print(items.count)
+    }
+
+}
+
+let url = Bundle.main.url(forResource: "test", withExtension: "pgn")!
+let string = try! String(contentsOf: url)
+let pgn = try! PGN(parse: string)
+let game = Game(pgn: pgn)
+let delegate = Delegate()
+game.delegate = delegate
+
+print(game.currentPosition.ascii)
+print(game.sanMoves)
+
+print(game.currentPosition.ascii)
+
+game.reverse(to: game.startIndex)
+print(game.currentPosition.ascii)
+game.advance(to: game.lastIndex)
+print(game.currentPosition.ascii)
+
+game.reverse(to: 15)
+print(game.currentPosition.ascii)
+
+game.advance(to: game.lastIndex)
+print(game.currentPosition.ascii)
 
 
-let eco = game.eco!.name
-    .replacingOccurrences(of: game.eco!.code.rawValue, with: "")
-    .trimmingCharacters(in: CharacterSet.whitespaces.union(CharacterSet.punctuationCharacters))
-print(">\(eco)<")
+
+
