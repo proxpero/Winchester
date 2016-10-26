b997733cf31300d01dc20eb8c3577ec6cc158357
Todd Olsen <todd.olsen@me.com>
2016-10-26 14:36:59 -0400

    Redesigned PieceNode functionality.

---
+++ Endgame/Sources/PieceNode.swift
@@ -0,0 +1,25 @@
+//
+//  PieceNode.swift
+//  Endgame
+//
+//  Created by Todd Olsen on 8/16/16.
+//  Copyright © 2016 Todd Olsen. All rights reserved.
+//
+
+import Engine
+import SpriteKit
+
+//final public class PieceNode: SKSpriteNode {
+//
+//    init(piece: Piece, with size: CGSize) {
+//        let imageName = "\(piece.color == .white ? "White" : "Black")\(piece.kind.name)"
+//        super.init(texture: SKTexture(imageNamed: imageName), color: .clear, size: size)
+//        self.name = String(piece.character)
+//        self.zPosition = 210
+//    }
+//
+//    required public init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//}
