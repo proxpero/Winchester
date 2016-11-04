996940683ecdc48cc7cf3f15348539129c5e0010
Todd Olsen <todd.olsen@me.com>
2016-11-04 09:52:54 -0400

    Created PGN-Tool

---
+++ PGN-Tool/PGN-Tool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PGN-Tool
+//
+//  Created by Todd Olsen on 11/3/16.
+//  Copyright © 2016 proxpero. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
