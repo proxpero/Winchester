1423a7801202ca2a626835c8858cdaf699703d02
Todd Olsen <todd.olsen@me.com>
2017-01-09 11:14:12 -0500

    Added new delegate method.

--- Endgame/Sources/GameDelegate.swift
+++ Endgame/Sources/GameDelegate.swift
@@ -12,4 +12,5 @@ public protocol GameDelegate: class {
     func game(_ game: Game, didAppend item: HistoryItem, at index: Int?)
     func game(_ game: Game, didTraverse items: [HistoryItem], in direction: Direction)
     func game(_ game: Game, didExecute move: Move, with capture: Capture?, with promotion: Piece?)
+    func game(_ game: Game, didEndWith outcome: Outcome)
 }
