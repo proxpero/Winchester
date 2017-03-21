b80a2887cbecd4f631b63fab0753940d5cd8932c
Todd Olsen <todd.olsen@me.com>
2017-03-21 13:41:16 -0400

    Add optionality to several properties.

--- Endgame/Sources/HistoryItem.swift
+++ Endgame/Sources/HistoryItem.swift
@@ -6,51 +6,31 @@
 //
 //
 
-// MARK: -
-
 /// The type of the element stored in a `game`'s `history` property.
-public struct HistoryItem: Equatable {
+public struct HistoryItem {
 
     // MARK: Stored Properties
 
     public let position: Position
-    public let move: Move
-    public let piece: Piece
+    public let move: Move?
+    public let piece: Piece?
     public let capture: Capture?
     public let promotion: Piece?
-    public let sanMove: String
+    public let sanMove: String?
 
-    // MARK: - Equatable Protocol Conformance
+}
 
+extension HistoryItem: Equatable {
     /// Returns `true` iff the two `HistoryItem` instances are the same.
     public static func == (lhs: HistoryItem, rhs: HistoryItem) -> Bool {
         return
             lhs.position == rhs.position &&
-            lhs.move == rhs.move &&
-            lhs.piece == rhs.piece &&
-            lhs.capture?.piece == rhs.capture?.piece &&
-            lhs.capture?.square == rhs.capture?.square &&
-            lhs.promotion == rhs.promotion &&
-            lhs.sanMove == rhs.sanMove
+                lhs.move == rhs.move &&
+                lhs.piece == rhs.piece &&
+                lhs.capture?.piece == rhs.capture?.piece &&
+                lhs.capture?.square == rhs.capture?.square &&
+                lhs.promotion == rhs.promotion &&
+                lhs.sanMove == rhs.sanMove
     }
 
-}
-
-// MARK: -
-
-/// The states a king could be in during a game.
-public enum KingStatus {
-    case safe
-    case checked
-    case checkmated
-
-    var algebraicAnnotation: String {
-        switch self {
-        case .checkmated:
-            return "#"
-        case .checked: return "+"
-        default:
-            return ""
-        }
-    }
 }
